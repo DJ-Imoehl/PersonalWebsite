0994c5148de912a13a42bee0128c8a6445bff804
DJ Imoehl <dj.imoehl@gmail.com>
2019-10-05 22:34:28 -0500

    Added smooth scrolling when the toolbar buttons are pressed, and adjusted script file to use document.ready instead of an onload function

--- scripts/scriptFile.js
+++ scripts/scriptFile.js
@@ -1,4 +1,4 @@
-function onPageLoad(){
+$(document).ready(function(){
   $(".resume-section-content").tilt({
     maxTilt: 5,
   });
@@ -7,4 +7,18 @@ function onPageLoad(){
     maxTilt: 5,
   });
 
-}
+  // Got this animation from w3schools: https://www.w3schools.com/howto/howto_css_smooth_scroll.asp#section2
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+
+      event.preventDefault();
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 500, function(){
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});

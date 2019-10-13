bba03baab4b1ff847e38d426fb56e980a7cc8f85
DJ Imoehl <dj.imoehl@gmail.com>
2019-10-13 11:53:21 -0500

    Fixed strange horizontal scroll bar issue on small screens, limited tilt functionality to screens greater than 300px in width, and deleted unnecessary classes

--- scripts/scriptFile.js
+++ scripts/scriptFile.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
-  $(".resume-section-content").tilt({
-    maxTilt: 5,
-  });
 
-  $(".opening-text-containter").tilt({
-    maxTilt: 5,
-  });
+  if($(this).width() > 300){
+    $(".resume-section-content").tilt({
+      maxTilt: 5,
+    });
+
+    $(".opening-text-containter").tilt({
+      maxTilt: 5,
+    });
+  }
 
   // Got this animation from w3schools: https://www.w3schools.com/howto/howto_css_smooth_scroll.asp#section2
   $("a").on('click', function(event) {
@@ -21,4 +24,25 @@ $(document).ready(function(){
       });
     } // End if
   });
+});
+
+
+$(window).resize(function() {
+  if ($(this).width() < 300) {
+    $(".resume-section-content").tilt({
+      maxTilt: 0,
+    });
+
+    $(".opening-text-containter").tilt({
+      maxTilt: 0,
+    });
+  } else {
+    $(".resume-section-content").tilt({
+      maxTilt: 5,
+    });
+
+    $(".opening-text-containter").tilt({
+      maxTilt: 5,
+    });
+  }
 });
